5e92946df269950a800a168ff05592cd4779ae5d
Nschulz88 <natalie.schulz@me.com>
2018-04-20 01:13:27 +0000

    Pictures are being diaplayed in chat

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
 
 class Message extends Component {
+
   render() {
     console.log('Rendering <Message/>');
-    let thisUsersColor = this.props.usercolor;
-    console.log(thisUsersColor);
+    let newContent = null;
+    let text = this.props.content;
+    let regex = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?\.(?:jpe?g|gif|png)/g;
+    let checkIfMatches = regex.test(text);
+    let imgURL = text.match(regex); // can be an array!!
+
+    if (checkIfMatches) {
+        newContent = imgURL.map((img) => {
+          return <img width="40%" src={img}/>
+        })
+    }
+
+    // style={{width: '80px' }}
 
     if (this.props.datatype === 'incomingNotification') {
       return (
@@ -13,13 +25,13 @@ class Message extends Component {
       </div>
       )
     }
-
+    
 
     return (
     <main className="messages">
         <div className="message">
-          <span className="message-username" style={{color: thisUsersColor }}>{this.props.username}</span>
-          <span className="message-content">{this.props.content}</span>
+          <span className="message-username" style={{color: this.props.usercolor }}>{this.props.username}</span>
+          <span className="message-content">{this.props.content} <br /> {newContent}</span>
         </div>
     </main>
     );
